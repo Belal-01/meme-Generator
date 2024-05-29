cd8a3c9ac975f7ce8897546227f1214f65f89f9b
Belal-01 <belalkhobieh343@gmail.com>
2024-05-29 15:09:17 +0300

    create the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Header from "./components/Header"
 function App() {
  return(
  <>
- <h1 className="title">hello</h1>
- <Header />
-
- 
+ <Header /> 
 </>)
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-
+ import Imglogo from "../../public/images/Troll Face.png"
 const Header = () => {
-  return (
-    <div>
-      <h1 className="Header">Header</h1>
+  
+  return (    
+  <nav className="nav">
+    <div className="nav__logo">
+      <img className="nav__logo--img" src={Imglogo} alt="" />
+      <h3 className="nav__logo--text">Meme Generator</h3>
     </div>
+    <div className="nav__title">
+      React Course - Project 3
+    </div>
+  </nav>
   )
 }
 
